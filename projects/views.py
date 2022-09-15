3786b4595b192da3db3a21d98146014054fbc284
Mokom Geh <mokomgeh45@gmail.com>
2022-09-15 02:41:19 -0400

    configured template directory & basic test views

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
+from curses.ascii import HT
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def projects(request):
+    return render(request, 'projects.html', context={})
+
+
+def project(request, pk):
+    return render(request, 'single-project.html', context={'pk':pk})
